e86c32c801c455eedf4ed4962435de4435dc3fab
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-09-01 21:24:20 +0530

    Instruction added in asset/text.txt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,6 @@ function App() {
   return (
     <div>
       <h1>App</h1>
-      <div>Sbse phle - git pull origin main - krna </div>
-      <div>git checkout develop - Isse develop branch pr switch ho jaoge aur isi par kaam krna</div>
-      <div>git .add - isse changes stage krna</div>
-      <div>git commit -m "Changes done" - Isse changes commit krna</div>
-      <div>git push -u origin develop - Isse origin pr push kr dena for time being</div>
-
-      <div>Jb kaam concrete aur stable ho jaaye tb</div>
-
-      <div>git checkout main - Isse main branch pr switch kr jaoge</div>
-      <div>git merge develop - Isse main branch merge ho jaege develop branch me </div>
-      <div>git push origin main - Fir main branch ko push kr dena</div>
     </div>    
   )
 }
